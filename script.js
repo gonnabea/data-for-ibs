158198c6ff2eb74c0885f905d9cf2d0185df46c9
Choi Jiwon <winning1993@naver.com>
2020-06-08 23:45:50 +0900

    데이터 완성, css

--- script.js
+++ script.js
@@ -1,27 +1,39 @@
+const data = require("./data");
 const searchInput = document.getElementById("searchInput"),
 searchBtn = document.getElementById("searchBtn"),
 searchArea = document.getElementById("searchArea"),
-background = document.getElementById("background")
+background = document.getElementById("background"),
+resultArea = document.getElementById("resultArea"),
+searchForm = document.getElementById("searchForm")
+
+console.log(data)
 
 function searchClick(){
-    searchBtn.style.animation = "moveBtn 0.3s forwards";
-    searchInput.style.animation = "focusSearch 0.3s forwards";
-    
+    searchBtn.style.animation = "moveBtn 0.4s forwards";
+    searchInput.style.animation = "focusSearch 0.4s forwards";
+    searchInput.style.textAlign = "center";
+    searchInput.placeholder = "증상 혹은 의심되는 상태를 입력하세요"
     setTimeout(()=> {
-        searchArea.addEventListener("click", revertInput);
-        background.addEventListener("click", revertInput);
+        resultArea.addEventListener("click", revertInput);
     }, 500)
 }
 
 function revertInput(){
-    searchBtn.style.animation = "revertBtn 0.3s forwards";
-    searchInput.style.animation = "outFocused 0.3s forwards";
-    searchArea.removeEventListener("click", revertInput);
-    background.removeEventListener("click", revertInput);
+    searchBtn.style.animation = "revertBtn 0.4s forwards";
+    searchInput.style.animation = "outFocused 0.4s forwards";
+    searchInput.style.textAlign = "start";
+    searchInput.placeholder = "증상을 입력하세요"
+    resultArea.removeEventListener("click", revertInput);
+}
+
+function handleSearch(e){
+    e.preventDefault();
+    revertInput()
 }
 
 function init(){
     searchInput.addEventListener("click", searchClick);
+    searchForm.addEventListener("submit", handleSearch)
 }
 
 init();
